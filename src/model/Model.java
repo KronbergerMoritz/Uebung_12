ff3294b33fcef61e2be2a85ac5d305da7fa3a8e2
Moritz Kronberger <moritzkronberger@aon.at>
2021-01-25 14:55:54 +0100

    Finished

--- src/model/Model.java
+++ src/model/Model.java
@@ -3,9 +3,9 @@ package model;
 public class Model
 {
 
-    private ModularCounter red;
-    private ModularCounter green;
-    private ModularCounter blue;
+    private ModularCounter red = new ModularCounter(0,256);
+    private ModularCounter green = new ModularCounter(0,256);
+    private ModularCounter blue = new ModularCounter(0,256);
 
     public Model()
     {
@@ -14,12 +14,27 @@ public class Model
 
     public void changColorViaAbsoluteValue(ColorCode cc, String value)
     {
-        
+
     }
 
     public void changColorViaAbsoluteValue(ColorCode cc, int value)
     {
-
+        if(cc == ColorCode.RED)
+        {
+            red.inc(value);
+        }
+        else if(cc == ColorCode.GREEN)
+        {
+            green.inc(value);
+        }
+        else if(cc == ColorCode.BLUE)
+        {
+            blue.inc(value);
+        }
+        else
+        {
+            System.out.println("Nochmal!");
+        }
     }
 
     public void changColorViaRelativeValue(ColorCode cc, String value)
@@ -27,8 +42,36 @@ public class Model
 
     }
 
-    public void changColorViaRelativeValue(ColorCode cc, int value)
-    {
+    public void changColorViaRelativeValue(ColorCode cc, int value) {
+        if (cc.equals(ColorCode.RED)) {
+            if (value < 0) {
+                value = value * (-1);
+                red.dec(value);
+            }
+            else
+            {
+                red.inc(value);
+            }
+        } else if (cc.equals(ColorCode.GREEN)) {
+            if (value < 0) {
+                value = value * (-1);
+                green.dec(value);
+            }
+            else
+            {
+                green.inc(value);
+            }
+        }
+        else if (cc.equals(ColorCode.BLUE)) {
+            if (value < 0) {
+                value = value * (-1);
+                blue.dec(value);
+            }
+            else
+            {
+                blue.inc(value);
+            }
+            }
 
     }
 
@@ -50,7 +93,7 @@ public class Model
         int g = getGreen();
         int b = getBlue();
 
-        String hex = String.format("#000000",r , g, b);
+        String hex = String.format("#%02x%02x%02x", r, g, b);
         return hex;
     }
 
---
+++ src/viewController/Controller.java
@@ -0,0 +1,145 @@
+package viewController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import model.ColorCode;
+import model.Model;
+
+public class Controller extends Model {
+
+    private Model model = new Model();
+
+    @FXML
+    private Label l_red;
+
+    @FXML
+    private TextField tf_red;
+
+    @FXML
+    private Label l_green;
+
+    @FXML
+    private TextField tf_green;
+
+    @FXML
+    private Label l_blue;
+
+    @FXML
+    private TextField tf_blue;
+
+    @FXML
+    private Button btn_red_add;
+
+    @FXML
+    private Button btn_red_sub;
+
+    @FXML
+    private Button btn_green_add;
+
+    @FXML
+    private Button btn_green_sub;
+
+    @FXML
+    private Button btn_blue_add;
+
+    @FXML
+    private Button btn_blue_sub;
+
+    @FXML
+    private TextField tf_hex;
+
+    @FXML
+    private Button btn_color;
+
+    @FXML
+    private void setBtn_color()
+    {
+
+        int blue = model.getBlue();
+
+        int green = model.getGreen();
+
+        int red = model.getRed();
+
+
+        tf_hex.setText(model.getHex());
+        btn_color.setStyle("-fx-background-color: rgb(" + red + ", " + green + ", " + blue + ");");
+    }
+
+    @FXML
+    private void setTf_blue()
+    {
+        if(!tf_blue.getText().equals("")) {
+            model.changColorViaAbsoluteValue(ColorCode.BLUE, Integer.parseInt(tf_blue.getText()));
+            setBtn_color();
+        }
+    }
+
+    @FXML
+    private void setTf_green()
+    {
+        if(!tf_green.getText().equals("")) {
+            model.changColorViaAbsoluteValue(ColorCode.GREEN, Integer.parseInt(tf_green.getText()));
+            setBtn_color();
+        }
+    }
+
+
+    @FXML
+    private void setTf_red()
+    {
+        if(!tf_red.getText().equals("")) {
+            model.changColorViaAbsoluteValue(ColorCode.RED, Integer.parseInt(tf_red.getText()));
+            setBtn_color();
+        }
+
+    }
+
+    @FXML
+    void blue_add(MouseEvent event)
+    {
+
+        model.changColorViaRelativeValue(ColorCode.BLUE, 10);
+        tf_blue.setText(Integer.toString(model.getBlue()));
+        setBtn_color();
+    }
+
+    @FXML
+    void blue_sub(MouseEvent event) {
+        model.changColorViaRelativeValue(ColorCode.BLUE, -10);
+        tf_blue.setText(Integer.toString(model.getBlue()));
+        setBtn_color();
+    }
+
+    @FXML
+    void green_add(MouseEvent event) {
+        model.changColorViaRelativeValue(ColorCode.GREEN, 10);
+        tf_green.setText(Integer.toString(model.getGreen()));
+        setBtn_color();
+    }
+
+    @FXML
+    void green_sub(MouseEvent event) {
+        model.changColorViaRelativeValue(ColorCode.GREEN, -10);
+        tf_green.setText(Integer.toString(model.getGreen()));
+        setBtn_color();
+    }
+
+    @FXML
+    void red_add(MouseEvent event) {
+        model.changColorViaRelativeValue(ColorCode.RED, 10);
+        tf_red.setText(Integer.toString(model.getRed()));
+        setBtn_color();
+    }
+
+    @FXML
+    void red_sub(MouseEvent event) {
+        model.changColorViaRelativeValue(ColorCode.RED, -10);
+        tf_red.setText(Integer.toString(model.getRed()));
+        setBtn_color();
+    }
+
+}
